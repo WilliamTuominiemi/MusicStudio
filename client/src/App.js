aad094cfc68c9127eabe6a8c43d2e60c542edadb
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-10-23 21:06:11 +0300

    canvas component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import './App.css'
+import Canvas from './components/Canvas.js'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '50vh',
+                width: '80vh',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                marginTop: '-25vh',
+                marginLeft: '-40vh',
+            }}
+        >
+            <Canvas />
+        </div>
+    )
 }
 
 export default App
