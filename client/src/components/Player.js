2540d3d0006b9396d34ea7ec16d7be7e04fed176
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-10-25 14:11:13 +0300

    stop button

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -5,45 +5,67 @@ import guitar from '../assets/guitar.mp3'
 import snare from '../assets/snare.mp3'
 
 const Player = (notes) => {
+    let timeouts = []
+
     const playSound = (note) => {
         let player = new Audio(note)
         player.playbackRate = 1.25
         player.play()
     }
 
-    const handleClick = () => {
+    const playButton = () => {
+        stop()
         const notesArray = Object.values(notes)
         if (notesArray.length > 0) {
             notesArray.forEach((note) => {
-                setTimeout(function () {
-                    switch (note.y) {
-                        case 1:
-                            playSound(piano)
-                            break
-                        case 2:
-                            playSound(guitar)
-                            break
-                        case 3:
-                            playSound(bass)
-                            break
-                        case 4:
-                            playSound(hihat)
-                            break
-                        case 5:
-                            playSound(snare)
-                            break
-                        default:
-                            break
-                    }
-                }, note.x * 500)
+                timeouts.push(
+                    setTimeout(function () {
+                        switch (note.y) {
+                            case 1:
+                                playSound(piano)
+                                break
+                            case 2:
+                                playSound(guitar)
+                                break
+                            case 3:
+                                playSound(bass)
+                                break
+                            case 4:
+                                playSound(hihat)
+                                break
+                            case 5:
+                                playSound(snare)
+                                break
+                            default:
+                                break
+                        }
+                    }, note.x * 500)
+                )
             })
+            console.log(timeouts)
         }
     }
 
+    const stop = () => {
+        for (let i = 0; i < timeouts.length; i++) {
+            clearTimeout(timeouts[i])
+        }
+
+        timeouts = []
+    }
+
     return (
-        <button onClick={handleClick} style={{ margin: '10px', padding: '10px' }}>
-            <img src="https://cdn-icons-png.flaticon.com/512/0/375.png" height="50px"></img>
-        </button>
+        <div>
+            <button onClick={playButton} style={{ margin: '10px', padding: '10px' }}>
+                <img src="https://cdn-icons-png.flaticon.com/512/0/375.png" height="50px"></img>
+            </button>
+            <button onClick={stop} style={{ margin: '10px', padding: '10px' }}>
+                <img
+                    src="https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/image-asset.gif"
+                    height="50px"
+                ></img>
+            </button>
+        </div>
     )
 }
 
