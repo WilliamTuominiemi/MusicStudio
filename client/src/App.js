b74b588ac2330680a8b43e14e1f1f6d05fca9463
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-10-25 10:54:51 +0300

    label for what sound is

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,13 @@ function App() {
                 height: '100vh',
             }}
         >
+            <div class="grid-container">
+                <div class="grid-item">PIANO</div>
+                <div class="grid-item">GUITAR</div>
+                <div class="grid-item">KICK</div>
+                <div class="grid-item">HIHAT</div>
+                <div class="grid-item">SNARE</div>
+            </div>
             <Canvas />
         </div>
     )
