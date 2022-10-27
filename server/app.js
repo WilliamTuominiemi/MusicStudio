3438458024fbaa78575f76fd16f4de62cd03d541
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-10-28 00:17:38 +0300

    server connected to mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
+const connectDb = require('./config/connectDb')
+
 const app = express()
+connectDb()
 
 app.use(express.json())
 app.use(cors())
---
+++ server/config/connectDb.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose')
+
+const connectDb = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.info('Connected to MongoDB')
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err}`)
+  }
+}
+
+module.exports = connectDb
