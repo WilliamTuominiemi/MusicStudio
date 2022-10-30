c3a3888cb1b8fd462c0966e7a28a8cde864a2fc3
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-10-30 14:04:46 +0200

    component for playing sound

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -5,11 +5,11 @@ import synth from '../assets/synth.mp3'
 import clap from '../assets/clap.mp3'
 import { useEffect, useState } from 'react'
 
+import { Play, Stop, Pitch, Speed } from './Sound'
+
 const Player = (notes) => {
     const [pitch, setPitch] = useState(1.0)
     const [speed, setSpeed] = useState(500)
-
-    let timeouts = []
 
     let playing = false
 
@@ -34,7 +34,6 @@ const Player = (notes) => {
             case 32:
                 if (playing) {
                     playing = false
-                    stop()
                 } else {
                     playing = true
                     playButton()
@@ -45,64 +44,22 @@ const Player = (notes) => {
         }
     }
 
-    const playSound = (note) => {
-        let player = new Audio(note)
-        player.playbackRate = pitch
-        player.play()
-    }
-
     const playButton = () => {
-        console.log(notes)
-
-        stop()
-        const notesArray = Object.values(notes)
-        if (notesArray.length > 0) {
-            notesArray.forEach((note) => {
-                timeouts.push(
-                    setTimeout(function () {
-                        switch (note.y) {
-                            case 1:
-                                playSound(bell)
-                                break
-                            case 2:
-                                playSound(synth)
-                                break
-                            case 3:
-                                playSound(bass)
-                                break
-                            case 4:
-                                playSound(hihat)
-                                break
-                            case 5:
-                                playSound(clap)
-                                break
-                            default:
-                                break
-                        }
-                    }, note.x * speed)
-                )
-            })
-        }
-    }
-
-    const stop = () => {
-        for (let i = 0; i < timeouts.length; i++) {
-            clearTimeout(timeouts[i])
-        }
-
-        timeouts = []
+        Play(notes)
     }
 
     const onScrollPitch = (e) => {
-        stop()
+        Stop()
         const delta = Math.round(e.deltaY * -0.001 * 1000) / 1000
         setPitch(Math.min(Math.max(Math.round((pitch + delta) * 1000) / 1000, 0.5), 5))
+        Pitch(pitch)
     }
 
     const onScrollSpeed = (e) => {
-        stop()
+        Stop()
         const delta = Math.round(e.deltaY * -0.1 * 1000) / 1000
         setSpeed(Math.min(Math.max(Math.round((speed + delta) * 1000) / 1000, 100), 1000))
+        Speed(speed)
     }
 
     return (
@@ -112,7 +69,7 @@ const Player = (notes) => {
                     <path d="M3 22v-20l18 10-18 10z" />
                 </svg>
             </button>
-            <button onClick={stop} style={{ margin: '10px', padding: '10px' }}>
+            <button onClick={Stop} style={{ margin: '10px', padding: '10px' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M2 2h20v20h-20z" />
                 </svg>
---
+++ client/src/components/Sound.js
@@ -0,0 +1,67 @@
+import bell from '../assets/bell.mp3'
+import bass from '../assets/808.mp3'
+import hihat from '../assets/hihat.mp3'
+import synth from '../assets/synth.mp3'
+import clap from '../assets/clap.mp3'
+
+let pitch = 1.0
+let speed = 500
+
+let timeouts = []
+
+let playing = false
+
+export const Play = (notes) => {
+    Stop()
+    const notesArray = Object.values(notes)
+    if (notesArray.length > 0) {
+        notesArray.forEach((note) => {
+            timeouts.push(
+                setTimeout(function () {
+                    switch (note.y) {
+                        case 1:
+                            playSound(bell)
+                            break
+                        case 2:
+                            playSound(synth)
+                            break
+                        case 3:
+                            playSound(bass)
+                            break
+                        case 4:
+                            playSound(hihat)
+                            break
+                        case 5:
+                            playSound(clap)
+                            break
+                        default:
+                            break
+                    }
+                }, note.x * speed)
+            )
+        })
+    }
+}
+
+const playSound = (note) => {
+    let player = new Audio(note)
+    player.playbackRate = pitch
+    player.play()
+}
+
+export const Stop = () => {
+    for (let i = 0; i < timeouts.length; i++) {
+        clearTimeout(timeouts[i])
+    }
+
+    timeouts = []
+}
+
+export const Pitch = (p) => {
+    pitch = p
+}
+
+export const Speed = (s) => {
+    console.log(s)
+    speed = s
+}
