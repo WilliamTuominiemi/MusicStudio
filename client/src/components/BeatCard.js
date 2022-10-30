c812b8a17a772a5d808c9e11d367b8e0bdbfbd28
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-10-30 21:17:26 +0200

    bootstrap

--- client/src/components/BeatCard.js
+++ client/src/components/BeatCard.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 
 import { Play, Stop, Pitch, Speed } from './Sound'
 
+import { Card } from 'react-bootstrap'
+
 export const BeatCard = (data) => {
     useEffect(() => {
         fixArray(data)
@@ -10,7 +12,6 @@ export const BeatCard = (data) => {
     const notes = []
 
     const fixArray = (data) => {
-        // console.log(data.beat.notes[0][1])
         data.beat.notes.forEach((note) => {
             notes.push(note[1])
         })
@@ -22,13 +23,15 @@ export const BeatCard = (data) => {
     }
 
     return (
-        <div>
-            <p>{data.beat._id}</p>
-            <button onClick={playButton} style={{ margin: '10px', padding: '10px' }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M3 22v-20l18 10-18 10z" />
-                </svg>
-            </button>
-        </div>
+        <Card className="text-center" style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title color="text.secondary">{data.beat._id}</Card.Title>
+                <button onClick={playButton} style={{ margin: '10px', padding: '10px' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M3 22v-20l18 10-18 10z" />
+                    </svg>
+                </button>
+            </Card.Body>
+        </Card>
     )
 }
