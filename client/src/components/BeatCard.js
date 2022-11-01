bfd7dba2e24b4746b33f80a0fb103eb3dde7cadb
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-11-01 22:45:22 +0200

    stop button in card

--- client/src/components/BeatCard.js
+++ client/src/components/BeatCard.js
@@ -31,6 +31,11 @@ export const BeatCard = (data) => {
                         <path d="M3 22v-20l18 10-18 10z" />
                     </svg>
                 </button>
+                <button onClick={Stop} style={{ margin: '10px', padding: '10px' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M2 2h20v20h-20z" />
+                    </svg>
+                </button>
             </Card.Body>
         </Card>
     )
